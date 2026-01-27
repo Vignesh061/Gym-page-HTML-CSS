ecb3fb181f3c3cf953fdd13688262fdbbea1f3df
Vignesh061 <vigneshvicky3611@gmail.com>
2026-01-27 23:15:22 +0530

    Create Navbar

---
+++ Rise-and-Rep/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './navbar'
+
+function App() {
+
+  return (  
+    // <>
+    // </>
+    <Navbar></Navbar>
+  )
+}
+
+export default App
---
+++ Rise-and-Rep/src/navbar.jsx
@@ -0,0 +1,17 @@
+function Navbar(){
+    return(
+        <nav className="nav">
+            <div className="brandname"> <a href="">Rise&Rep</a></div>
+            <ul className="nav-links">
+                <li> <a href="">Home</a> </li>
+                <li> <a href="">Programs</a> </li>
+                <li> <a href="">Trainer</a> </li>
+                <li> <a href="">About</a> </li>
+                <li> <a href="">Contect</a> </li>
+            </ul>
+        </nav>
+
+    );
+}
+
+export default Navbar
