1d84ec7446a6d78356052411e2f1f347643b9f2e
Vignesh061 <vigneshvicky3611@gmail.com>
2026-01-30 23:11:51 +0530

    Modified Navbar

--- Rise-and-Rep/src/navbar.jsx
+++ Rise-and-Rep/src/navbar.jsx
@@ -1,15 +1,24 @@
 function Navbar(){
     return(
         <nav className="nav">
-            <div className="brandname"> <a href="">Rise&Rep</a></div>
-            <ul className="nav-links">
-                <li> <a href="">Home</a> </li>
-                <li> <a href="">Programs</a> </li>
-                <li> <a href="">Trainer</a> </li>
-                <li> <a href="">About</a> </li>
-                <li> <a href="">Contect</a> </li>
-            </ul>
+            <div className="nav-container">
+
+                <div className="brandname"> 
+                    <a href="">Rise&Rep</a>
+                </div>
+                <ul className="nav-links">
+                    <li> <a href="">Home</a> </li>
+                    <li> <a href="">Programs</a> </li>
+                    <li> <a href="">Trainer</a> </li>
+                    <li> <a href="">About</a> </li>
+                    <li> <a href="">Contect</a> </li>
+                </ul>
+
+            </div>
         </nav>
+
+
+        
 
     );
 }
